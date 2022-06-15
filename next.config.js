b0c23e97df7e67ecbdb14ef509fe199a8281dd6f
Griko Nibras <git@griko.id>
2022-06-15 23:56:52 +0700

    feat(next): disable eslint and ts checks

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const withTranspileModules = require('next-transpile-modules');
 
 /** @type {import("next").NextConfig} */
 let nextConfig = {
-  //
+  eslint: {
+    ignoreDuringBuilds: Boolean(process.env.VERCEL),
+  },
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: Boolean(process.env.VERCEL),
+  },
 };
 
 const localModules = ['rescript', ...bsConfig['bs-dependencies']];
